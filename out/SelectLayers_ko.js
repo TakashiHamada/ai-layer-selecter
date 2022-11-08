98da6432b71200e745e151c911eec722de36b307
TakashiHamada <hamada@gift10.net>
2022-11-08 15:21:38 +0900

    ai関連ファイル（アクション等）

---
+++ out/SelectLayers_ko.js
@@ -0,0 +1,27 @@
+(function(){
+    
+    // "let" will be error in ai2023
+    var TARGET_IDX = 4;
+    //                0      1      2        3      4     5     6     7
+    var languages = ["ja", "en", "zh-tw", "zh-cn", "ko", "es", "fr", "de"]; 
+    
+    var textLayer = getLayer("texts");
+    var baseLayer = getLayer("bases");
+    
+    if (textLayer === null || baseLayer === null)
+        return;
+    
+    for (var idx = 0; idx < languages.length; idx++) {
+        textLayer.layers.getByName(languages[idx]).visible = idx === TARGET_IDX;
+        baseLayer.layers.getByName(languages[idx]).visible = idx === TARGET_IDX;
+    }
+    
+    // --
+    function getLayer(name) {
+        try{
+            return  app.activeDocument.layers.getByName(name);
+        }catch(error){
+            return null;
+        }
+    }
+})()
